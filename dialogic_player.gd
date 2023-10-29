6e332380fa43e5e92f887956b1a51c5e5839c872
Aaron Rawson <aaronmrawson@gmail.com>
2023-10-29 13:37:45 -0400

    signals are pretty cool

--- dialogic_player.gd
+++ dialogic_player.gd
@@ -1,4 +1,11 @@
 extends Node2D
 
 func _ready():
+    Dialogic.timeline_ended.connect(_on_dialogic_timeline_ended)
+
     Dialogic.start('letagirlread')
+
+
+func _on_dialogic_timeline_ended():
+    $Icon.visible = true
+    print(Dialogic.VAR.TestGroup.AfterSchoolOption)
