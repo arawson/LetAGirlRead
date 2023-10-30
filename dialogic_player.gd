cdeeea54336fbbfaeb9d7929748b5ba3c57702de
Aaron Rawson <aaronmrawson@gmail.com>
2023-10-30 07:46:05 -0400

    some stuff

--- dialogic_player.gd
+++ dialogic_player.gd
@@ -1,11 +1,24 @@
 extends Node2D
 
 func _ready():
-    Dialogic.timeline_ended.connect(_on_dialogic_timeline_ended)
+	Dialogic.text_signal.connect(_on_dialogic_text_signal)
+	Dialogic.signal_event.connect(_on_dialogic_signal_event)
+	Dialogic.timeline_ended.connect(_on_dialogic_timeline_ended)
 
-    Dialogic.start('letagirlread')
+	Dialogic.start('letagirlread')
 
 
 func _on_dialogic_timeline_ended():
-    $Icon.visible = true
-    print(Dialogic.VAR.TestGroup.AfterSchoolOption)
+	$Icon.visible = true
+	print(Dialogic.VAR.TestGroup.AfterSchoolOption)
+
+
+func _on_dialogic_text_signal(text: String):
+	print("text signal " + text)
+
+
+func _on_dialogic_signal_event(argument: String):
+	print("event signal " + argument)
+	match argument:
+		"knife":
+			$BloodSplatter.visible = true
